a24f746b1e556dc41253e8120d8f70aaf6c48664
ARSENLUPIN <mdoukali007@gmail.com>
2021-07-30 04:34:08 +0100

    imageOK

--- src/Components/Molecules/SectionLogoLogin/index.js
+++ src/Components/Molecules/SectionLogoLogin/index.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import { Image } from 'react-bootstrap';
-import Image from 'react-bootstrap/Image'
+//import logo512 from './logo512.png'; 
+//import Image from 'react-bootstrap/Image'
+import logo from '../../../logo.svg';
 import SectionloginLogoStc from "./SectionLogoLogin.stc";
 
 const SectionLogoLogin = () => {
 
     return ( 
         <SectionloginLogoStc>
-            <div className="row py-5 rounded">
-                <div className="col-lg-md-5">
-                    <Image src="logo512.png" rounded />
-                </div>
-            </div>
+            <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
         </SectionloginLogoStc>
     
 
