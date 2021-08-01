a3cb4d036108e0f5d4455975b685d82bcaec021f
ARSENLUPIN <mdoukali007@gmail.com>
2021-08-02 00:38:45 +0100

    ImagePasPosition

--- src/Components/Molecules/SectionLogoLogin/SectionLogoLogin.stc.js
+++ src/Components/Molecules/SectionLogoLogin/SectionLogoLogin.stc.js
@@ -2,8 +2,15 @@ import Styled from 'styled-components';
 import { Row } from 'reactstrap';
 
 const SectionloginLogoStc=Styled(Row)`
-    background: #36393e;
+    
     border-raduis: .8rem;
 
+.app{
+    background-image: url("/LogoCrm.jpg");
+    border-raduis: .8rem;
+    width: 34rem;
+    height: 40rem;
+    background-position: center;
+}
 `;
 export default SectionloginLogoStc;
--- src/Components/Molecules/SectionLogoLogin/index.js
+++ src/Components/Molecules/SectionLogoLogin/index.js
@@ -2,32 +2,18 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 //import { Image } from 'react-bootstrap';
 //import logo512 from './logo512.png'; 
-//import Image from 'react-bootstrap/Image'
-import logo from '../../../logo.svg';
+//import LogoCrm from '../../../../public/LogoCrm.jpg';
+//import logo from '../../../logo.svg';
 import SectionloginLogoStc from "./SectionLogoLogin.stc";
 
 const SectionLogoLogin = () => {
 
     return ( 
         <SectionloginLogoStc>
-            <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+            <div className="app">
+              
+            </div>
         </SectionloginLogoStc>
-    
 
         )
 }
